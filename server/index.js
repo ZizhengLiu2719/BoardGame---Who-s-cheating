e3f03a645061dd901e0b3f65649398007eddf886
ZizhengLiu2719 <zl024@email.uark.edu>
2025-06-14 11:52:19 -0500

    a

--- server/index.js
+++ server/index.js
@@ -15,13 +15,68 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'titlePage.html'));
 });
 
+// Room management
+const rooms = {};
+
+// Helper: get player list for a room
+function getPlayerList(roomId) {
+  return rooms[roomId]?.players.map(p => p.name) || [];
+}
+
 // Socket.IO connection handler
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  // You will add your game logic here
+  // Host creates a room
+  socket.on('createRoom', ({ roomId, password, maxPlayers, hostName }, callback) => {
+    if (rooms[roomId]) {
+      return callback({ success: false, message: 'Room already exists.' });
+    }
+    rooms[roomId] = {
+      password,
+      maxPlayers: Number(maxPlayers),
+      players: [{ id: socket.id, name: hostName }],
+    };
+    socket.join(roomId);
+    callback({ success: true });
+    io.to(roomId).emit('playerListUpdate', getPlayerList(roomId));
+  });
 
+  // Player joins a room
+  socket.on('joinRoom', ({ roomId, password, playerName }, callback) => {
+    const room = rooms[roomId];
+    if (!room) {
+      return callback({ success: false, message: 'Room does not exist.' });
+    }
+    if (room.password !== password) {
+      return callback({ success: false, message: 'Incorrect password.' });
+    }
+    if (room.players.length >= room.maxPlayers) {
+      return callback({ success: false, message: 'Room is full.' });
+    }
+    if (room.players.some(p => p.name === playerName)) {
+      return callback({ success: false, message: 'Name already taken in this room.' });
+    }
+    room.players.push({ id: socket.id, name: playerName });
+    socket.join(roomId);
+    callback({ success: true });
+    io.to(roomId).emit('playerListUpdate', getPlayerList(roomId));
+  });
+
+  // Handle disconnect
   socket.on('disconnect', () => {
+    for (const [roomId, room] of Object.entries(rooms)) {
+      const idx = room.players.findIndex(p => p.id === socket.id);
+      if (idx !== -1) {
+        room.players.splice(idx, 1);
+        io.to(roomId).emit('playerListUpdate', getPlayerList(roomId));
+        // Remove room if empty
+        if (room.players.length === 0) {
+          delete rooms[roomId];
+        }
+        break;
+      }
+    }
     console.log('User disconnected:', socket.id);
   });
 });
